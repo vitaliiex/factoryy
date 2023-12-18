dc5697dca2a46e22c4cc9ad3464c2491796e07d4
vitaliiex <151929039+vitaliiex@users.noreply.github.com>
2023-12-19 00:40:43 +0300

    Update Program.cs

--- factory/Program.cs
+++ factory/Program.cs
@@ -1,10 +1,78 @@
-﻿namespace factory
+﻿namespace FabricPriclad
 {
+    interface ILang
+    {
+        void Display();
+    }
+    class L1 : ILang
+    {
+        public void Display()
+        {
+            Console.WriteLine("Мова програмування C#");
+        }
+    }
+    class L2 : ILang
+    {
+        public void Display()
+        {
+            Console.WriteLine("Мова програмування Python");
+        }
+    }
+    class L3 : ILang
+    {
+        public void Display()
+        {
+            Console.WriteLine("Мова програмування Java");
+        }
+    }
+    interface IFactory
+    {
+        ILang CreateLang();
+    }
+    class L1Factory : IFactory
+    {
+        public ILang CreateLang()
+        {
+            return new L1();
+        }
+    }
+    class L2Factory : IFactory
+    {
+        public ILang CreateLang()
+        {
+            return new L2();
+        }
+    }
+    class L3Factory : IFactory
+    {
+        public ILang CreateLang()
+        {
+            return new L3();
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Введіть число 1,2 або 3: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            switch (x)
+            {
+                case 1:
+                    IFactory factory = new L1Factory();
+                    ILang lang = factory.CreateLang();
+                    lang.Display();
+                    break;
+                case 2:
+                    IFactory factory2 = new L2Factory();
+                    ILang lang2 = factory2.CreateLang();
+                    lang2.Display(); break;
+                case 3:
+                    IFactory factory3 = new L3Factory();
+                    ILang lang3 = factory3.CreateLang();
+                    lang3.Display(); break;
+                default: Console.WriteLine("Ni"); break;
+            }
         }
     }
 }
